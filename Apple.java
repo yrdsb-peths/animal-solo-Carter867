f60e792337a4f92753f7912fb18c8641425734c4
Carter867 <carterylkwong@gmail.com>
2025-04-29 17:08:17 -0400

    Part 1 Completed.

---
+++ Apple.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * food for elephant
+ * 
+ * @author (carter) 
+ * @version (april 29)
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x,y);
+        
+        //remove apple ad draw game over when apple gets to bottom
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+}
