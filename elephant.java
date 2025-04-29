2538498cf6acd1f208cda49e9217c107a4e4cb01
Carter867 <carterylkwong@gmail.com>
2025-04-29 17:37:56 -0400

    .

--- elephant.java
+++ elephant.java
@@ -9,6 +9,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public elephant()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
