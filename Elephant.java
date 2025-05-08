ab7b543c1067df93b66e117cf53152441ded4777
Carter867 <carterylkwong@gmail.com>
2025-05-08 09:15:41 -0400

    Update Elephant.java

--- Elephant.java
+++ Elephant.java
@@ -9,7 +9,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
     
     public void act()
     {
